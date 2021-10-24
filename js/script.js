7400e910385a8d2ce9c85af79e71025937cbdce9
Olga <motyolya@gmail.com>
2021-10-24 22:17:31 +0300

    Auto-changing
    Added auto-changing of images and a timer for it

--- js/script.js
+++ js/script.js
@@ -1,25 +1,32 @@
+const timeTextArea = document.getElementById("time");
 let slideIndex = 1;
-showSlides(slideIndex);
+let timeout;
+let time = 2000;
+showSlidesByHand(slideIndex)
+
+function handOnClick() {
+  clearTimeout(timeout);
+}
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlidesByHand(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlidesByHand(slideIndex = n);
 }
 
-function showSlides(n) {
+function showSlidesByHand(n) {
   let i;
   const slides = document.getElementsByClassName("mySlides");
   const dots = document.getElementsByClassName("dot");
 
   if (n > slides.length) {
-    slideIndex = 1
+    slideIndex = 1;
   }
 
   if (n < 1) {
-    slideIndex = slides.length
+    slideIndex = slides.length;
   }
 
   for (i = 0; i < slides.length; i++) {
@@ -31,4 +38,21 @@ function showSlides(n) {
   
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
+}
+
+function showSlides() {
+  if (timeTextArea.value != '' && timeTextArea.value != 0) {
+    time = timeTextArea.value * 1000;
+  }
+
+  clearTimeout(timeout);
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    timeout = setTimeout(showSlides, time);
 }
